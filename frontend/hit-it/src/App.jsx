7f68a5169642841f4ff8f96a2b294ca250416a4e
dberos <sdi1600269@di.uoa.gr>
2022-05-17 22:28:20 +0300

    add Welcome

--- frontend/hit-it/src/App.jsx
+++ frontend/hit-it/src/App.jsx
@@ -1,11 +1,13 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 // import Login from "./pages/Login";
 // import Register from "./pages/Register";
+import Welcome from "./pages/Welcome";
 
 const App = () => {
-  return <Home/>
+  // return <Home/>
   //return <Register/>
   // return <Login/>
+  return <Welcome/>
 };
 
 export default App;
---
+++ frontend/hit-it/src/pages/Welcome.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container=styled.div`
+    background-color:#eaeded; 
+    width:100%;
+    height:700px;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    position:relative;
+`;
+
+const Logo=styled.h1`
+    font-weight:900;
+    color:#e67e22; 
+    font-size:70px;
+    position:absolute;
+    margin-bottom:350px;
+    margin-right:800px;
+`;
+
+const Description=styled.p`
+    font-size:25px;
+    font-family: 'Arial', sans-serif;
+    margin-top:80px;
+    font-weight:500;
+    margin-right:650px;
+    margin-bottom:200px;
+    position:absolute;
+    width:450px;
+`;
+
+const Button=styled.button`
+    font-family: 'Arial', sans-serif;
+    position:absolute;
+    height:50px;
+    background-color:#e67e22;
+    border-radius:10px;
+    border:none;
+    &:hover{
+        border:2px;
+        border-style:solid;
+        border-color:grey;
+    }
+    margin-left:900px;
+    margin-top:350px;
+    font-weight:700;
+`;
+
+const Welcome = () => {
+  return (
+    <Container>
+        <Logo>
+            hit-it
+        </Logo>
+        <Description>
+            Ότι θες θα το βρεις στο hit-it
+        </Description>
+        <Button>
+            Συνέχεια σαν επισκέπτης
+        </Button>
+    </Container>
+  )
+}
+
+export default Welcome
