a974af77c8a97006a27cde511940a0ec57489028
dberos <sdi1600269@di.uoa.gr>
2022-05-27 02:23:40 +0300

    functionality at categories/search

--- frontend/hit-it/src/components/Categories/NavBar.jsx
+++ frontend/hit-it/src/components/Categories/NavBar.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { ShoppingCartOutlined } from "@material-ui/icons"
 import { Badge } from '@material-ui/core';
 import { Search } from '@material-ui/icons';
 import { useNavigate } from 'react-router-dom';
+import { search_results } from '../../data';
 
 const Container=styled.div`
     height:80px;
+    position:relative;
 `;
 
 const Wrapper=styled.div`
@@ -33,21 +35,22 @@ const Logo=styled.h1`
     cursor:pointer;
 `;
 
+
 const SearchContainter=styled.div`
     &:hover{
         border:1px;
         border-style:solid;
         border-color:#e67e22;
     }
-    margin-top:10px;
-    flex:1;
     display:flex;
     align-items:center;
-    justify-content:center;
     width:30%;
     border-radius:25px;
     padding:5px;
+    position:absolute;
     background-color:white;
+    margin-top:60px;
+    margin-left:30%;
 `;
 
 const Input=styled.input`
@@ -58,6 +61,29 @@ const Input=styled.input`
     ::placeholder{
         opacity:0.1;
     }
+`;
+
+const DummyDiv=styled.div`
+    position:absolute;
+    width:30%;
+    margin-left:30%;
+
+`;
+
+const ResultContainer=styled.div`
+    width:100%;
+    background-color:white;
+    z-index:1;
+    margin-top:50px;
+    position:absolute;
+    border-radius:10px;
+`;
+
+const Result=styled.div`
+    margin-left:10px;
+    margin-top:10px;
+    margin-bottom:10px;
+    font-family: 'Arial', sans-serif;
 `;
 
 const Right=styled.div`
@@ -78,6 +104,21 @@ const NavBar = () => {
     
     let navigate=useNavigate();
 
+    const [filtered_data,set_filtered_data]=useState([]);
+
+    const handle_filter=(event)=>{
+        const search_word=event.target.value;
+        const new_filter=search_results.filter((value)=>{
+            return value.title.toLowerCase().includes(search_word.toLowerCase());
+        })
+        if(search_word===""){
+            set_filtered_data([]);
+        }
+        else{
+            set_filtered_data(new_filter);
+        }
+    }
+
   return (
     <Container>
         <Wrapper>
@@ -87,9 +128,23 @@ const NavBar = () => {
                 </Logo>
             </Left>
             <SearchContainter>
-                <Input placeholder="γράψε τον όρο αναζήτησης"/>
-                <Search style={{color:"#e67e22",fontSize:25}} />
-            </SearchContainter>
+            <Input placeholder="γράψε τον όρο αναζήτησης" onChange={handle_filter}/>
+            <Search style={{color:"#e67e22",fontSize:25}} />
+        </SearchContainter>
+
+
+        {filtered_data.length !==0 && (
+            <DummyDiv>
+            <ResultContainer>
+                {filtered_data.map((value)=>
+                    {return(
+                        <Result key={value.id} target="_blank">
+                            {value.title}
+                        </Result>
+                    );})}
+            </ResultContainer>
+            </DummyDiv>
+        )}
             <Right>
                 <MenuItem>
                     Register
