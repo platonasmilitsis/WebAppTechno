57cfe096f60960f6d716115c7ac5c5981c310055
dberos <sdi1600269@di.uoa.gr>
2022-07-13 16:29:51 +0300

    Spring initializr

---
+++ backend/hit-it/src/test/java/com/hitit/HitItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
