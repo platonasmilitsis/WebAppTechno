7a44b9aea237c03fbeeb80853fa216e74942e7a3
dberos <sdi1600269@di.uoa.gr>
2022-07-15 15:54:19 +0300

    new Spring initializr

---
+++ backend/hit-it/src/main/java/com/hitit/HitItApplication.java
@@ -0,0 +1,13 @@
+package com.hitit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitItApplication.class, args);
+	}
+
+}
