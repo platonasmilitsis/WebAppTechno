5784994797ef0f53764e35d425ce97bfd9ffac2b
dberos <sdi1600269@di.uoa.gr>
2022-07-28 13:33:03 +0300

    better

--- frontend/hit-it/src/components/Home/Announcement.jsx
+++ frontend/hit-it/src/components/Home/Announcement.jsx
@@ -1,73 +1,3 @@
-// import styled from "styled-components"
-
-// const Container=styled.div`
-//     height:300px;
-//     background-color:#e67e22;
-//     display:flex;
-//     align-items:center;
-//     font-size:15px;
-//     font-weight:bold;
-//     margin-left:285px;
-//     margin-right:285px;
-//     border-radius:10px;
-//     position:relative;
-//     max-width:1275px;
-//     -drag: none;
-//     user-select: none;
-//     -moz-user-select: none;
-//     -webkit-user-drag: none;
-//     -webkit-user-select: none;
-//     -ms-user-select: none;
-// `;
-
-// const Title=styled.h1`
-//     font-size:33px;
-//     font-family: 'Arial', sans-serif;
-//     margin-bottom:80px;
-//     margin-left:50px;
-//     position:absolute;
-// `;
-
-// const Description=styled.p`
-//     font-size:17px;
-//     font-family: 'Arial', sans-serif;
-//     margin-top:80px;
-//     margin-left:50px;
-//     position:absolute;
-//     width:450px;
-
-// `;
-
-// const ImgContainer=styled.div`
-//     margin-left:70%;
-//     overflow:hidden;
-//     position:absolute;
-// `;
-
-// const Image=styled.img`
-//     height:100%;
-//     width:100%;
-//     object-fit:cover;
-// `;
-
-// const Announcement = () => {
-//   return (
-//     <Container>
-//         <Title>
-//           Ήρθαν τα hit-it Point
-//         </Title>
-//         <Description>
-//           Τώρα μπορείς να παραλάβεις την παραγγελία σου, όποτε σε βολεύει!
-//         </Description>
-//         <ImgContainer>
-//           <Image src={require("../../assets/logoreact.png")}/>
-//         </ImgContainer>
-//     </Container>
-//   )
-// }
-
-// export default Announcement
-
 import React from 'react'
 import styled from 'styled-components'
 
@@ -81,7 +11,12 @@ const Container=styled.div`
   width:70%;
   margin-left:15%;
   margin-right:15%;
-  @media (max-width: 1000px) {
+  @media (max-width: 1000px) and (min-width:600px){
+    width:90%;
+    margin-left:5%;
+    margin-right:5%;
+  }
+  @media screen and (max-width: 601px){
     width:100%;
     margin-left:0;
     margin-right:0;
@@ -120,7 +55,6 @@ const Description=styled.h2`
 `;
 
 const ImageContainer=styled.div`
-  ${'' /* background-color:blue; */}
   height:300px;
   width:300px;
 `;
