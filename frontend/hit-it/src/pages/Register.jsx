4084f9639c7142b7f81b4d32930cb79bddf57635
dberos <sdi1600269@di.uoa.gr>
2022-08-22 15:04:43 +0300

    perfected register functionality

--- frontend/hit-it/src/pages/Register.jsx
+++ frontend/hit-it/src/pages/Register.jsx
@@ -1,9 +1,13 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
 import { useNavigate,Link} from 'react-router-dom';
+import CheckIcon from '@mui/icons-material/Check';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const PageContainer=styled.div`
   height:200px;
+  padding-top:10px;
+  background-color:#eaeded; 
 `;
 
 const Logo=styled.h1`
@@ -28,23 +32,44 @@ const RegisterText=styled.h2`
 `;
 
 const Wrapper = styled.div`
-  width: 25%;
   padding: 20px;
   border-radius:10px;
   background-color: white;
-  min-width:400px;
+  position:relative;
+  @media (max-width: 650px) {
+    width:400px;
+  }
+  display:flex;
 `;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
+  margin-left:15px;
 `;
 
-const Input = styled.input`
-  flex: 1;
-  min-width: 40%;
-  margin: 8px 0;
-  padding: 10px;
+const InputContainer=styled.div`
+    display:flex;
+    align-items:center;
+    width:600px;
+    flex-wrap:wrap;
+    @media (max-width: 650px) {
+      width:380px;
+    }
+`;
+
+const Input=styled.input`
+    height:30px;
+    font-size:18px;
+    ::placeholder{
+        opacity:0.3;
+        font-size:14px;
+        margin-left:50px;
+    }
+    width:570px;
+    @media (max-width: 650px) {
+    width:350px;
+  }
 `;
 
 const ButtonDiv=styled.div`
@@ -67,8 +92,9 @@ const Button = styled.button`
 const ErrorMessage=styled.p`
   color:red;
   font-size:12px;
-  margin-bottom:5px;
+  margin-top:55px;
   margin-left:5px;
+  position:absolute;
 `;
 
 const Field=styled.p`
@@ -77,11 +103,18 @@ const Field=styled.p`
   font-weight:50;
   opacity:0.8;
   margin-left:5px;
-
+  margin-bottom:7px;
+  margin-top:17px;
 `;
 
 const LinkDiv=styled.div`
   margin-top:-10px;
+`;
+
+const IconDiv=styled.div`
+  color:green;
+  margin-left:2px;
+
 `;
 
 const Register = () => {
@@ -101,12 +134,16 @@ const Register = () => {
   const [errors,set_errors]=useState({});
   const [used_email,set_used_email]=useState(null);
   const [used_username,set_used_username]=useState(null);
+  const [notifications,set_notifications]=useState({});
+  const [user_icon,set_user_icon]=useState(false);
+  const [mail_icon,set_mail_icon]=useState(false);
 
   const handle_submit=(e)=>{
     e.preventDefault();
     set_used_email(null);
     set_used_username(null);
     set_errors(validate());
+    set_notifications({});
   }
 
   const validate=()=>{
@@ -124,7 +161,7 @@ const Register = () => {
       errors.email="Το Email είναι υποχρεωτικό";
     }
     if(!tin){
-      errors.tin="Το ΑΦΜ είναι υποχρεωτικό";
+      errors.tin="Ο ΑΦΜ είναι υποχρεωτικός";
     }
     if(!password){
       errors.password="Ο Κωδικός Πρόσβασης είναι υποχρεωτικός";
@@ -136,7 +173,7 @@ const Register = () => {
       errors.confirm_password="Οι Κωδικοί Πρόσβασης πρέπει να ταιριάζουν";
     }
     if(isNaN(tin)){
-      errors.tin="Το ΑΦΜ πρέπει να είναι αριθμός";
+      errors.tin="O ΑΦΜ πρέπει να είναι αριθμός";
     }
     if(telephone && isNaN(telephone)){
       errors.telephone="Το τηλέφωνο πρέπει να είναι αριθμός";
@@ -149,19 +186,211 @@ const Register = () => {
     return errors;
   }
 
-  const register=()=>{
+  const check_first_name=(str)=>{
+    set_first_name(str);
+    if(str.length===0){
+      notifications.first_name=false;
+      errors.first_name="Το Όνομα είναι υποχρεωτικό";
+    }
+    else{
+      errors.first_name=null;
+      notifications.first_name=true;
+    }
+  }
+
+  const check_last_name=(str)=>{
+    set_last_name(str);
+    if(str.length===0){
+      notifications.last_name=false;
+      errors.last_name="Το Επώνυμο είναι υποχρεωτικό"
+    }
+    else{
+      errors.last_name=null;
+      notifications.last_name=true;
+    }
+  }
+
+  const check_tin=(str)=>{
+    set_tin(str);
+    if(str.length===0){
+      notifications.tin=false;
+      errors.tin="Ο ΑΦΜ είναι υποχρεωτικός"
+    }
+    else{
+      if(!isNaN(str)){
+        errors.tin=null;
+        notifications.tin=true;
+      }
+      else{
+        notifications.tin=false;
+        errors.tin="Ο ΑΦΜ πρέπει να είναι αριθμός";
+      }
+    }
+  }
+
+  const check_username=(str)=>{
+    set_username(str);
+    fetch("http://localhost:8080/users")
+    .then(response=>response.json())
+    .then((data)=>{
+      try{
+        const user_name=data.find(data=>data.username===str).username;
+        if(user_name){
+          set_used_username("Το Όνομα Χρήστη "+str+" χρησιμοποιείται ήδη");
+          errors.username=null;
+          set_user_icon(false);
+        }
+      }
+      catch(error){
+        set_used_username(null);
+        errors.username=null;
+        if(str){
+          set_user_icon(true);
+        }
+        else{
+          set_user_icon(false);
+          errors.username="Το Όνομα Χρήστη είναι υποχρεωτικό";
+        }
+      }
+    })
+    .catch((error)=>{
+      console.error(error);
+      navigate("/error");
+    })
+  }
+
+  const check_email=(str)=>{
+    set_email(str);
+    /* eslint-disable no-useless-escape */
+    let mail_format=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(str && str.match(mail_format)){
+      fetch("http://localhost:8080/users")
+      .then(response=>response.json())
+      .then((data)=>{
+        try{
+          const e_mail=data.find(data=>data.email===str).email;
+          if(e_mail){
+            set_used_email("Το Email "+str+" χρησιμοποιείται ήδη");
+            errors.email=null;
+            set_mail_icon(false);
+          }
+        }
+        catch(error){
+          set_used_email(null);
+          errors.email=null;
+          set_mail_icon(true);
+        }
+      })
+      .catch((error)=>{
+        console.error(error);
+        navigate("/error");
+      })
+    }
+    else if(str && !str.match(mail_format)){
+      set_used_email(null);
+      set_mail_icon(false);
+      errors.email=null;
+    }
+    else{
+      set_mail_icon(false);
+      errors.email="Το Email είναι υποχρεωτικό";
+    }
+  }
+
+  const check_password=(str)=>{
+    set_password(str);
+    if(str.length===0){
+      notifications.password=false;
+      errors.password="Ο Κωδικός Πρόσβασης είναι υποχρεωτικός";
+    }
+    else{
+      notifications.password=true;
+      errors.password=false;
+      if(confirm_password){
+        if(str===confirm_password){
+          errors.confirm_password=null;
+          notifications.confirm_password=true;
+        }
+        else{
+          notifications.confirm_password=false;
+          errors.confirm_password="Οι Κωδικοί Πρόσβασης πρέπει να ταιριάζουν";
+        }
+      }
+    }
+  }
+
+  const check_confirm=(str)=>{
+    set_confirm_password(str);
+    if(str.length===0){
+      if(password){
+        notifications.confirm_password=false;
+        errors.confirm_password="Η Επιβεβαίωση Κωδικού είναι υποχρεωτική";
+      }
+      else{
+        notifications.confirm_password=false;
+        errors.confirm_password=null;
+      }
+    }
+    else{
+      if(password){
+        if(str===password){
+          notifications.confirm_password=true;
+          errors.confirm_password=null;
+        }
+        else{
+          notifications.confirm_password=false;
+          errors.confirm_password="Οι Κωδικοί Πρόσβασης πρέπει να ταιριάζουν";
+        }
+      }
+    }
+  }
+
+  const check_telephone=(str)=>{
+    set_telephone(str);
+    if(str){
+      if(isNaN(str)){
+        errors.telephone="Το τηλέφωνο πρέπει να είναι αριθμός";
+        notifications.telephone=false;
+      }
+      else{
+        errors.telephone=null;
+        notifications.telephone=true;
+      }
+    }
+    else{
+      errors.telephone=null;
+      notifications.telephone=false;
+    }
+  }
+
+  const check_address=(str)=>{
+    // No regex yet
+    set_address(str);
+    if(str){
+      errors.address=null;
+      notifications.address=true;
+    }
+    else{
+      errors.address=null;
+      notifications.address=false;
+    }
+  }
+
+  const register=(e)=>{
+    e.preventDefault();
+    set_errors(validate());
     const data ={
-                username:username, 
-                first_name:first_name,
-                last_name:last_name,
-                email:email,
-                tin:tin,
-                password:password===confirm_password?password:null,
-                telephone:telephone,
-                address:address,
-                admin:false,
-                accepted:false
-                };
+              username:username, 
+              first_name:first_name,
+              last_name:last_name,
+              email:email,
+              tin:tin,
+              password:password===confirm_password?password:null,
+              telephone:telephone,
+              address:address,
+              admin:false,
+              accepted:false
+              };
 
     fetch('http://localhost:8080/users', {
       method: 'POST',
@@ -175,19 +404,7 @@ const Register = () => {
     .then((data) => {
       console.log(data);
       try{
-        if(data.message[0]==="$"){
-          set_used_email("Το Email "+email+" χρησιμοποιείται ήδη");
-          set_used_username("Το Όνομα Χρήστη "+username+" χρησιμοποιείται ήδη");
-        }
-        else if(data.message[0]==="E"){
-          set_used_email("Το Email "+email+" χρησιμοποιείται ήδη");
-          console.log(email);
-        }
-        else if(data.message[0]==="U"){
-          set_used_username("Το Όνομα Χρήστη "+username+" χρησιμοποιείται ήδη");
-          console.log(username);
-        }
-        console.log(data.message);
+        console.log(data.message[0]);
       }
       catch(error){
         // Won't redirect if everything is not solved
@@ -208,7 +425,15 @@ const Register = () => {
 
   return (
     <PageContainer>
-    <Container>
+      <Container>
+        <HelmetProvider>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>
+                    Εγγραφή
+                </title>
+            </Helmet>
+        </HelmetProvider>
         <Logo>
             hit-it
         </Logo>
@@ -216,66 +441,134 @@ const Register = () => {
           Δημιουργία Λογαριασμού
         </RegisterText>
         <Wrapper>
-            <Form onSubmit={handle_submit}>
-                <Field>
-                  Όνομα*
-                </Field>
-              <Input type="text" placeholder="Εισάγετε το Όνομά σας" onChange={(e)=>{set_first_name(e.target.value)}}/>
-              <ErrorMessage>{errors.first_name}</ErrorMessage>
-                <Field>
-                  Επώνυμο*
-                </Field>
-              <Input type="text" placeholder="Εισάγετε το Επώνυμό σας" onChange={(e)=>{set_last_name(e.target.value)}}/>
-              <ErrorMessage>{errors.last_name}</ErrorMessage>
-                <Field>
-                  Όνομα Χρήστη*
-                </Field>
-              <Input type="text" placeholder="Εισάγετε το Όνομα Χρήστη" onChange={(e)=>{set_username(e.target.value)}}/>
-              <ErrorMessage>{errors.username}</ErrorMessage>
-              <ErrorMessage>{used_username}</ErrorMessage>
-                <Field>
-                  Email*
-                </Field>
-              <Input type="text" placeholder="Εισάγετε το Email σας" onChange={(e)=>{set_email(e.target.value)}}/>
-              <ErrorMessage>{errors.email}</ErrorMessage>
-              <ErrorMessage>{used_email}</ErrorMessage>
-                <Field>
-                  ΑΦΜ*
-                </Field>
-              <Input type="text" placeholder="Εισάγετε το ΑΦΜ σας" onChange={(e)=>{set_tin(e.target.value)}}/>
-              <ErrorMessage>{errors.tin}</ErrorMessage>
-                <Field>
-                  Κωδικός Πρόσβασης*
-                </Field>
-              <Input type="password" placeholder="Εισάγετε τον Κωδικό Πρόσβασης" onChange={(e)=>{set_password(e.target.value)}}/>
-              <ErrorMessage>{errors.password}</ErrorMessage>
-                <Field>
-                  Επιβεβαίωση Κωδικού*
-                </Field>
-              <Input type="password" placeholder="Επαναλάβετε τον Κωδικό Πρόσβασης" onChange={(e)=>{set_confirm_password(e.target.value)}}/>
-              <ErrorMessage>{errors.confirm_password}</ErrorMessage>
-                <Field>
-                  Τηλέφωνο
-                </Field>
-              <Input type="text" placeholder="Εισάγετε το Τηλέφωνό σας" onChange={(e)=>{set_telephone(e.target.value)}}/>
-              <ErrorMessage>{errors.telephone}</ErrorMessage>
-                <Field>
-                  Διεύθυνηση
-                </Field>
-              <Input type="text" placeholder="Εισάγετε τη Διεύθυνσή σας" onChange={(e)=>{set_address(e.target.value)}}/>
-              <ButtonDiv>
-                <Button onClick={register}>
-                  Εγγραφή
-                </Button>
-              </ButtonDiv>
-              <LinkDiv>
-              <Link to={"/"}  style={{color: "black"}}>
-                  Σύνδεση
-              </Link>
-              </LinkDiv>
-            </Form>
+          <Form onSubmit={handle_submit}>
+            <Field>
+              Όνομα*
+            </Field>
+            <InputContainer>
+              <Input type="text" placeholder="Εισάγετε το Όνομά σας" onChange={(e)=>{check_first_name(e.target.value)}}/>
+              {!notifications.first_name?
+                  <ErrorMessage>{errors.first_name}</ErrorMessage>:
+                <IconDiv>
+                  <CheckIcon fontSize='small'/>
+                </IconDiv>
+                }
+            </InputContainer>
+            <Field>
+              Επώνυμο*
+            </Field>
+            <InputContainer>
+              <Input type="text" placeholder="Εισάγετε το Επώνυμό σας" onChange={(e)=>{check_last_name(e.target.value)}}/>
+              {!notifications.last_name?
+                <ErrorMessage>{errors.last_name}</ErrorMessage>:
+                <IconDiv>
+                  <CheckIcon fontSize='small'/>
+                </IconDiv>
+                }
+            </InputContainer>
+            <Field>
+              Όνομα Χρήστη*
+            </Field>
+            <InputContainer>
+              <Input type="text" placeholder="Εισάγετε το Όνομα Χρήστη" onChange={(e)=>{check_username(e.target.value)}}/>
+              {!user_icon?
+              <>
+                <ErrorMessage>{errors.username}</ErrorMessage>
+                <ErrorMessage>{used_username}</ErrorMessage>
+              </>:
+              <IconDiv>
+                  <CheckIcon fontSize='small'/>
+                </IconDiv>
+              }
+            </InputContainer>
+            <Field>
+              Email*
+            </Field>
+            <InputContainer>
+              <Input type="text" placeholder="Εισάγετε το Email σας" onChange={(e)=>{check_email(e.target.value)}}/>
+              {!mail_icon?
+                <>
+                <ErrorMessage>{errors.email}</ErrorMessage>
+                <ErrorMessage>{used_email}</ErrorMessage>
+                </>:
+                <IconDiv>
+                  <CheckIcon fontSize='small'/>
+                </IconDiv>
+              }
+            </InputContainer>
+            <Field>
+              ΑΦΜ*
+            </Field>
+            <InputContainer>
+              <Input type="text" placeholder="Εισάγετε το ΑΦΜ σας" onChange={(e)=>{check_tin(e.target.value)}}/>
+              {!notifications.tin?
+                  <ErrorMessage>{errors.tin}</ErrorMessage>:
+                <IconDiv>
+                  <CheckIcon fontSize='small'/>
+                </IconDiv>
+              }
+            </InputContainer>
+            <Field>
+              Κωδικός Πρόσβασης*
+            </Field>
+            <InputContainer>
+              <Input type="password" placeholder="Εισάγετε τον Κωδικό Πρόσβασης" onChange={(e)=>{check_password(e.target.value)}}/>
+              {!notifications.password?
+                <ErrorMessage>{errors.password}</ErrorMessage>:
+                <IconDiv>
+                  <CheckIcon fontSize='small'/>
+                </IconDiv>
+                }
+              </InputContainer>
+            <Field>
+              Επιβεβαίωση Κωδικού*
+            </Field>
+            <InputContainer>
+              <Input type="password" placeholder="Επαναλάβετε τον Κωδικό Πρόσβασης" onChange={(e)=>{check_confirm(e.target.value)}}/>
+              {!notifications.confirm_password?
+                <ErrorMessage>{errors.confirm_password}</ErrorMessage>:
+                <IconDiv>
+                  <CheckIcon fontSize='small'/>
+                </IconDiv>
+              }
+            </InputContainer>
+            <Field>
+              Τηλέφωνο
+            </Field>
+            <InputContainer>
+              <Input type="text" placeholder="Εισάγετε το Τηλέφωνό σας" onChange={(e)=>{check_telephone(e.target.value)}}/>
+              {!notifications.telephone?
+                <ErrorMessage>{errors.telephone}</ErrorMessage>:
+                <IconDiv>
+                  <CheckIcon fontSize='small'/>
+                </IconDiv>
+              }
+            </InputContainer>
+            <Field>
+              Διεύθυνση
+            </Field>
+            <InputContainer>
+              <Input type="text" placeholder="Εισάγετε τη Διεύθυνσή σας" onChange={(e)=>{check_address(e.target.value)}}/>
+              {!notifications.address?
+                <ErrorMessage>{errors.address}</ErrorMessage>:
+                <IconDiv>
+                  <CheckIcon fontSize='small'/>
+                </IconDiv>
+              }
+            </InputContainer>
+            <ButtonDiv>
+              <Button onClick={register}>
+                Εγγραφή
+              </Button>
+            </ButtonDiv>
+            <LinkDiv>
+            <Link to={"/"}  style={{color: "black"}}>
+                Σύνδεση
+            </Link>
+            </LinkDiv>
+          </Form>
         </Wrapper>
-    </Container>
+      </Container>
     </PageContainer>
   );
 };
